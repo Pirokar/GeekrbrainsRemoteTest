c78de97e6d6e4207fe24589761c33a3a9959efe3
Vladimir Rybalka <emessage@yandex.ru>
2020-12-17 21:57:13 +0300

    First commit

---
+++ app/src/main/java/com/geekbrains/a1l1_helloworld/MainActivity.java
@@ -0,0 +1,38 @@
+package com.geekbrains.a1l1_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        setOnBtnClickBehavior();
+    }
+
+    private void initViews() {
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+    }
+
+    private void setOnBtnClickBehavior() {
+        button.setOnClickListener(v -> {
+            int color = ContextCompat.getColor(getApplicationContext(), R.color.black);
+            textView.setText(getString(R.string.another_text));
+            textView.setTextColor(color);
+        });
+
+        String[] months = getResources().getStringArray(R.array.month);
+    }
+}
